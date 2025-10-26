1e4f0da958c39c4352f0e0c88d9ca1706386a564
rogerseleri246-png <rogerseleri246@gmail.com>
2025-10-26 15:47:19 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
-const CACHE = 'valuecalc-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './icon-192.png', './icon-512.png'];
+// sw.js — v3
+const CACHE = 'valuecalc-v3';
+const ASSETS = ['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
 
