75b2d575496fde45c55c09cf0f556c3a33eaa5e2
rogerseleri246-png <rogerseleri246@gmail.com>
2025-10-26 15:14:40 +0000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'valuecalc-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
